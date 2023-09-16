10465525281aaddb46e8637006b69aed2cbdd7d6
Ishan Aryan <toxic.valorant.yt@gmail.com>
2023-09-16 14:51:10 +0530

    added the car to book section and animation to it

--- app.js
+++ app.js
@@ -34,54 +34,61 @@ addClassWhenNear(".ow", "showup", 5);
 let logo = document.querySelector(".logo");
 logo.style.transform = "translateY(0%)";
 
-
 let faqwrap = document.querySelector(".faq-wrap");
 let faq = document.querySelector("#faq");
-window.addEventListener('scroll',()=>{
+window.addEventListener("scroll", () => {
   let rect = faqwrap.getBoundingClientRect();
-  if(rect.top < window.innerHeight){
+  if (rect.top < window.innerHeight) {
     faq.classList.add("da-an");
-  }
-  else{
+  } else {
     faq.classList.remove("da-an");
   }
 });
 
-let daWrap = document.querySelector('.da-wrapper');
-let da = document.querySelector('.da');
-window.addEventListener('scroll', () => {
+let daWrap = document.querySelector(".da-wrapper");
+let da = document.querySelector(".da");
+window.addEventListener("scroll", () => {
   let rect = daWrap.getBoundingClientRect();
-  let mart = rect.top +80;
+  let mart = rect.top + 80;
   if (mart < window.innerHeight) {
     da.classList.add("da-an");
-  }
-  else{
+  } else {
     da.classList.remove("da-an");
   }
 });
 
 let footer = document.querySelector("footer");
 let footerclass = document.querySelector(".footer");
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   let rect = footer.getBoundingClientRect();
   let mart = rect.top + 50;
-  if ( mart< window.innerHeight) {
+  if (mart < window.innerHeight) {
     footerclass.classList.add("da-an");
-  }
-  else{
+  } else {
     footerclass.classList.remove("da-an");
   }
 });
 
 let contactForm = document.querySelector(".contact-form");
 let cfc = document.querySelector(".cfc");
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   let rect = contactForm.getBoundingClientRect();
   let mart = rect.top + 50;
-  if ( mart< window.innerHeight) {
+  if (mart < window.innerHeight) {
     cfc.classList.add("da-an");
-  }
-  else{
+  } else {
     cfc.classList.remove("da-an");
+  }
+});
+
+let ctbc = document.querySelector(".ctbc");
+let ctb = document.querySelector(".ctb");
+window.addEventListener("scroll", () => {
+  let rect = ctbc.getBoundingClientRect();
+  let mart = rect.top + 100;
+  if (mart < window.innerHeight) {
+    ctb.classList.add("da-an");
+  } else {
+    ctb.classList.remove("da-an");
   }
 });
